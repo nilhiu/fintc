1abe903f32a9fcb700642ed6cc5ca98e6f3cba29
nilhiu <heresy@disroot.org>
2025-03-31 16:35:08 +0000

    Implemented the 'init' command.

--- fintc.sh
+++ fintc.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 # FINTC_HOME is where all fintc files will reside (including file hashes)
 FINTC_HOME=${FINTC_HOME:-"/var/lib/fintc"}
+FINTC_HASHES=$(if [[ -f "$FINTC_HOME/hashes" ]]; then cat "$FINTC_HOME/hashes"; else echo ""; fi)
 
 # print_usage - prints the usage text of fintc
 print_usage() {
@@ -29,16 +30,60 @@ ensure_dep() {
     fi
 }
 
+# hash_file - hashes a file with BLAKE3 and places it into fintc's hashes
+#
+# Arguments:
+#   file: the file you want to hash
+hash_file() {
+    local file=$1
+    local hash=$(b3sum $file)
+
+    while read line; do
+        if [[ $(echo $line | awk '{ print $2 }') = $file ]]; then
+            echo "[error]: file '$file' is already hashed."
+            if [[ "${line%% *}" != "${hash%% *}" ]]; then
+                echo "[info]: hash mismatch. not updating..."
+            fi
+            return
+        fi
+    done <<< "$FINTC_HASHES"
+
+    echo "[info]: file '$file' hashed"
+    echo "$hash" >> "$FINTC_HOME/hashes"
+}
+
+# init_cmd - grabs hashes of the file(s) given
+#
+# Arguments:
+#   file/dir: the file or directory to hash
+init_cmd() {
+    if [[ -d $1 ]]; then
+        local dir=$1
+        local files=$(find $dir -type f -exec realpath {} \;)
+        for item in $files; do
+            if [[ -f $item ]]; then
+                hash_file $item
+            fi
+        done
+    elif [[ -f $1 ]]; then
+        local file=$(realpath $1)
+        hash_file $file
+    fi
+}
+
 ensure_dep b3sum
 
-if [[ $# -eq 0 ]]; then
+if [[ $# -ne 2 ]]; then
     print_usage
     exit 1
 fi
 
+mkdir -p "$FINTC_HOME"
+touch "$FINTC_HOME/hashes"
+
 case ${1,,} in
     init)
-        echo "init command detected"
+        init_cmd $2
         ;;
     verify)
         echo "verify command detected"
